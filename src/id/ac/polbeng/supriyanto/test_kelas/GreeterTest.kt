bb22bf11f4ce59b1fe3ad8215f7a85cd178d11d0
ACER <supriyantosupri0205@gmail.com>
2025-10-03 13:03:24 +0700

    madul3: Oop

--- src/id/ac/polbeng/supriyanto/test_kelas/GreeterTest.kt
+++ src/id/ac/polbeng/supriyanto/test_kelas/GreeterTest.kt
@@ -1,16 +1,7 @@
 package id.ac.polbeng.supriyanto.test_kelas
 
-/*fun main (){
+fun main (){
     val greeter = Greeter()
     greeter.greet()
-}*/
-
-fun main() {
-    val greeter = Greeter()
-    greeter.greet()
-    greeter.text = "Hi"
-    greeter.greet("Anton")
-    greeter.greet("Budi")
-    greeter.text = "Hello programmer"
-    println(greeter.with_ret_val("Dono"))
 }
+
